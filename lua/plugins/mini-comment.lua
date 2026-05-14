42b5ebe54f24f6d2cde9a824be50a1ee5ca22fd8
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-14 19:59:03 +0200

    Comment

---
+++ lua/plugins/mini-comment.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-mini/mini.comment",
+    version = "*",
+    event = "VeryLazy",
+
+    opts = {
+      options = {
+        ignore_blank_line = false,
+        start_of_line = false,
+      },
+
+      mappings = {
+        comment = "gc",
+        comment_line = "gcc",
+        comment_visual = "gc",
+        textobject = "gc",
+      },
+    },
+  },
+}
