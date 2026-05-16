891a807b260a418e985716e406903b02c6944d6a
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-16 16:14:42 +0200

    Improved overseer keymap

--- lua/keymap/r-run.lua
+++ lua/keymap/r-run.lua
@@ -2,9 +2,17 @@ local map = require("keymap.util").map
 
 local M = {}
 
+local function run_task()
+	require("overseer").run_task({
+		search_params = {
+			dir = vim.fn.getcwd(),
+		},
+	})
+end
+
 function M.setup()
-	map("n", "<leader>rr", "<cmd>OverseerRun<cr>", "Run task")
-	map("n", "<leader>rt", "<cmd>OverseerToggle right<cr>", "Toggle tasks")
+	map("n", "<leader>rr", run_task, "Run task")
+	map("n", "<leader>rt", "<cmd>OverseerTaskAction<cr>", "Task actions")
 end
 
 return M
