3136390bde0328e52bbc7cdcbb2fca6776002f74
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-16 16:52:24 +0200

    Improved todo stuff

--- lua/keymap/x-trouble.lua
+++ lua/keymap/x-trouble.lua
@@ -17,7 +17,20 @@ function M.setup()
 	end, "Location list")
 	map("n", "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", "Symbols")
 	map("n", "<leader>xr", "<cmd>Trouble lsp_references toggle focus=false<cr>", "LSP references")
-	map("n", "<leader>xt", "<cmd>Trouble todo toggle<cr>", "Todo comments")
+	map("n", "<leader>xt", function()
+		require("todo-comments.fzf").todo({
+			keywords = {
+				"TODO",
+				"NOTE",
+			},
+			prompt = "Todo> ",
+			previewer = false,
+			winopts = {
+				title = " TODO List ",
+				title_flags = false,
+			},
+		})
+	end, "Todo comments")
 end
 
 return M
