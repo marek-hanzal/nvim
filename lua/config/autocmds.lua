d071805cc08414c74a7707243a47f26c532b50e8
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-18 16:47:18 +0200

    Improved terminal stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,6 +3,53 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+local function terminal_cfile()
+	local token = vim.fn.expand("<cWORD>")
+
+	if token == "" then
+		return nil, nil
+	end
+
+	token = token:gsub("^['\"(<%[]+", ""):gsub("[>'\"%)%]},;]+$", "")
+
+	local path, line = token:match("^(.-):(%d+):%d+$")
+
+	if path then
+		return path, tonumber(line)
+	end
+
+	path, line = token:match("^(.-):(%d+)$")
+
+	if path then
+		return path, tonumber(line)
+	end
+
+	return vim.fn.expand("<cfile>"), nil
+end
+
+local function open_terminal_file(opts)
+	opts = opts or {}
+
+	local file, line = terminal_cfile()
+
+	if not file or file == "" then
+		vim.notify("No file under cursor", vim.log.levels.WARN)
+		return
+	end
+
+	vim.cmd("stopinsert")
+
+	if vim.fn.winnr("#") > 0 then
+		vim.cmd("wincmd p")
+	end
+
+	vim.cmd("edit " .. vim.fn.fnameescape(file))
+
+	if opts.line and line then
+		vim.api.nvim_win_set_cursor(0, { line, 0 })
+	end
+end
+
 autocmd("TextYankPost", {
 	group = augroup("highlight_yank", { clear = true }),
 	callback = function()
@@ -18,5 +65,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 		vim.cmd("Neotree toggle filesystem reveal left")
 		vim.cmd("wincmd p")
+	end,
+})
+
+autocmd("TermOpen", {
+	group = augroup("terminal_file_nav", { clear = true }),
+	callback = function(args)
+		local map_opts = {
+			buffer = args.buf,
+			silent = true,
+		}
+
+		vim.keymap.set({ "n", "t" }, "gf", function()
+			open_terminal_file()
+		end, vim.tbl_extend("force", map_opts, {
+			desc = "Open file under cursor in previous window",
+		}))
+
+		vim.keymap.set({ "n", "t" }, "gF", function()
+			open_terminal_file({ line = true })
+		end, vim.tbl_extend("force", map_opts, {
+			desc = "Open file under cursor and jump to line in previous window",
+		}))
 	end,
 })
